fa82d68cc90c7e5b008baa1082b51474282e3969
pprosser-redhat <pprosser@redhat.com>
2020-04-22 13:42:29 +0100

    removed v2

--- src/main/java/com/phil/members/rest/MemberResourceRESTService.java
+++ src/main/java/com/phil/members/rest/MemberResourceRESTService.java
@@ -50,7 +50,8 @@ import com.phil.members.service.MemberRegistration;
  * <p/>
  * This class produces a RESTful service to read/write the contents of the members table.
  */
-@Path("v2/members")
+//@Path("v2/members")
+@Path("/members")
 @RequestScoped
 public class MemberResourceRESTService {
 
